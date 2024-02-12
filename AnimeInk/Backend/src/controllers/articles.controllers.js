a3325c546eb812cb0269fe789a3f6b7730500630
Pierre Maze <promazelaygue@gmail.com>
2024-02-12 19:05:29 +0100

    Add first part for methode POST (functionnel with repoonse 201)

--- AnimeInk/Backend/src/controllers/articles.controllers.js
+++ AnimeInk/Backend/src/controllers/articles.controllers.js
@@ -1,4 +1,4 @@
-import { getAllArticles } from "../databases/articles.db.js";
+import { getAllArticles, findArticle } from "../databases/articles.db.js";
 
 export const GetAllArticles = async (req, res) => {
   try {
@@ -10,5 +10,35 @@ export const GetAllArticles = async (req, res) => {
     }
   } catch (error) {
     res.status(500).send("All Articles NOT FOUND");
+  }
+};
+
+export const CreateArticle = async (req, res) => {
+  const { api_url } = req.body;
+  const article = api_url;
+
+  // Vérifier si toutes les données requises sont fournies
+  if (!article) {
+    return res.status(400).json({ error: "Article is required" });
+  }
+
+  try {
+    // Vérifier si l'article existe déjà
+    const existingArticle = await findArticle(article);
+
+    if (existingArticle.result) {
+      // Si l'article existe déjà, utiliser les données de l'article existant
+      // Nous pouvons ajouter votre logique spécifique ici, par exemple, ajouter un commentaire à l'article existant
+      return res.status(200).json({
+        message: "Article already exists",
+        article: existingArticle.result,
+      });
+    } else {
+      // Si l'article n'existe pas, créer un nouvel article
+      // Ajouter votre logique pour créer un nouvel article ici
+      return res.status(201).json({ message: "Article created successfully" });
+    }
+  } catch (error) {
+    return res.status(500).send("Error creating article");
   }
 };
--- AnimeInk/Backend/src/databases/articles.db.js
+++ AnimeInk/Backend/src/databases/articles.db.js
@@ -16,3 +16,38 @@ export const getAllArticles = async () => {
     return { error, result };
   }
 };
+
+// Fonction pour vérifier si l'article existe déjà
+export const findArticle = async (api_url) => {
+  const sql = `
+    SELECT * FROM articles WHERE api_url = ?`;
+  const values = [api_url];
+
+  let error = null;
+  let result = null;
+
+  try {
+    result = await query(sql, values);
+  } catch (err) {
+    error = err.message;
+  } finally {
+    return { error, result };
+  }
+};
+
+export const addArticle = async (articles) => {
+  const { api_url } = articles;
+  try {
+    const result = await connection
+      .promise()
+      .query("INSERT INTO category (name, color, font_color) VALUES (?)", [
+        api_url,
+      ]);
+    if (result.affectedRows === 1) {
+      return result.insertId;
+    }
+    return category;
+  } catch (error) {
+    throw new Error("Failed to CREATE Category");
+  }
+};
