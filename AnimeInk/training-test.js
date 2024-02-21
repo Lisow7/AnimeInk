39fae5d32aa1f331b2602b6592c30d01a4328cc8
Antoine PC Portable <antoinemazelaygue33@outlook.com>
2024-02-21 10:37:34 +0100

    First fonction JS, a simple addition to the first fonction

---
+++ AnimeInk/training-test.js
@@ -0,0 +1,13 @@
+// Faire une fonction tous les jours differents (à chaque fois en dessous de l'ancienne, sans changer les anciennes), git add, git commit -m "text explicatif et le numero de la fonction et la date" (creer fonction n°1, n°2, etc...), push, faire une PR (Pierre en review et je m'assinge + le label "training et exchanges daily, verification (PR)").
+
+// Finir la maquette en responssive, faire les wireframes et les models ! ( exemple sur discord )
+
+// Fonction N°1
+// Déclaration d'une fonction fléchée basique qui prend deux paramètres et renvoie leur somme:
+
+const addition = (a, b) => {
+  return a + b;
+};
+
+// Utilisation de la fonction
+console.log(addition(3, 5)); // Affiche 8
