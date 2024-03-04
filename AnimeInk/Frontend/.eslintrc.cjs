329b3fdc170fb8512446bf5c8f096f362d7a3921
Pierre Maze <promazelaygue@gmail.com>
2024-03-04 20:20:07 +0100

    first commit

---
+++ AnimeInk/Frontend/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "no-console": "off",
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector:
+          "CallExpression[callee.object.name='console'][callee.property.name!=/^(error|warn|info)$/]",
+        message:
+          "You can only call the error(), warn() and info() methods from the console object",
+      },
+    ],
+  },
+};
---
+++ AnimeInk/Frontend/src/components/Picture.jsx
@@ -0,0 +1,13 @@
+const Picture = () => {
+  return (
+    <>
+      <img
+        src="https://img.freepik.com/photos-premium/porte-kaminarimon-lumiere-nuit-du-temple-sensoji-ji-quartier-asakusa-tokyo_43737-404.jpg?w=740"
+        alt="Japon"
+        className="start"
+      />
+    </>
+  );
+};
+
+export default Picture;
