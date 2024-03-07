5c78f6b8cf2a1226a684fd433fb06dc53a9275a6
Pierre M. <promazelaygue@gmail.com>
2024-03-07 05:50:51 +0100

    New: Add MVC for update password with hash & salt before stock in bdd

--- AnimeInk/Backend/src/controllers/accounts.controllers.js
+++ AnimeInk/Backend/src/controllers/accounts.controllers.js
@@ -1,4 +1,4 @@
-import { register, login } from "../databases/accounts.db.js";
+import { register, login, updatePassword } from "../databases/accounts.db.js";
 import { compareHash } from "../utils/crypto.utils.js";
 import { generateToken } from "../middlewares/jwt.mdlwr.js";
 import saveToken from "../utils/tokens.utils.js";
@@ -74,5 +74,40 @@ export const Login = async (req, res) => {
       message: "Internal Server Error 🚫",
       error: error.message,
     });
+  }
+};
+
+export const UpdatePassword = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const response = await updatePassword(
+      {
+        hashedPassword: req.hashedPassword,
+      },
+      id
+    );
+
+    console.info("Password hashed", req.hashedPassword);
+
+    if (!response) {
+      res.status(404).json({
+        success: false,
+        message: "Password is NOT UPDATED ❌",
+      });
+      return console.error(response, "Error Model ! 🚧");
+    }
+
+    res.status(202).json({
+      success: true,
+      message: "Password is UPDATED Successfully ✅",
+      password: response.hashedPassword,
+    });
+
+    return [response.hashedPassword];
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error 🚫", error });
   }
 };
