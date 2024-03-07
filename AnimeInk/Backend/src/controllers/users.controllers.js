f12d5da69eff4f44c0bab5c076fc74bb673a0675
Pierre M. <promazelaygue@gmail.com>
2024-03-07 04:15:45 +0100

    Update Model and controller User for use dynamic query with map and the function Object.values || Object.keys

--- AnimeInk/Backend/src/controllers/users.controllers.js
+++ AnimeInk/Backend/src/controllers/users.controllers.js
@@ -1,21 +1,46 @@
-import { updateProfile } from "../databases/users.db.js";
+import { updateProfile, deleteAvatar } from "../databases/users.db.js";
 
 export const UpdateProfile = async (req, res) => {
   try {
     const { id } = req.params;
+    const { username, email, avatar } = req.body;
 
-    const response = await updateProfile(req.body, id);
-    if (response) {
-      res
-        .status(202)
-        .send({ success: true, message: "Profile UPDATED", data: response });
-    } else {
-      res.status(404).send({
+    const response = await updateProfile(username, email, avatar, id);
+    if (!response) {
+      res.status(404).json({
         success: false,
         message: "Profile NOT UPDATED",
       });
+      return console.error(response, "Error Model ! ❌");
     }
+    res.status(202).json({
+      success: true,
+      message: "Profile UPDATED",
+      username: username,
+      email: email,
+      avatar: avatar,
+    });
+    return [response.username, response.email, response.avatar];
   } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
+    res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error 🚫", error });
+  }
+};
+
+export const DeleteAvatar = async (req, res) => {
+  try {
+    const user_id = req.params.id;
+    const response = await deleteAvatar(user_id);
+    if (!response) {
+      res.status(404).json({ success: false, message: "User NOT DELETE" });
+      return false;
+    }
+    res.status(202).json({ success: true, message: "User DELETE on Success" });
+    return true;
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error 🚫", error });
   }
 };
--- AnimeInk/Backend/src/databases/users.db.js
+++ AnimeInk/Backend/src/databases/users.db.js
@@ -1,20 +1,48 @@
 import query from "../databases/init.db.js";
 
-export const updateProfile = async (userData, user_id) => {
-  const { username, email, avatar } = userData;
+export const updateProfile = async (user, user_id) => {
+  // Extraction des clés et des valeurs de l'objet user
+  const keys = Object.keys(user);
+  const values = Object.values(user);
 
-  const sql = `UPDATE users SET username = ?, email = ?, avatar = ? WHERE user_id = ?`;
+  // Construction de la requête SQL pour mettre à jour les valeurs du profil avec la variable keys on utilise le .map() pour générer un new array sur lequel on va mapper tous les champs et valeur de la table users.
+  let sql = "UPDATE users SET ";
+  const updateClauses = keys.map((key) => `${key} = ?`);
+  sql += updateClauses.join(", ");
+  sql += " WHERE user_id = ?"; // Adaptation de l'ID de l'utilisateur à user_id
+
+  // Ajout de l'ID de l'utilisateur à la liste des valeurs
+  values.push(user_id);
 
   let error = null;
   let result = null;
 
   try {
-    result = await query(sql, [username, email, avatar, user_id]);
+    result = await query(sql, values);
 
     if (result.affectedRows === 1) {
-      return userData;
+      return user_id;
     }
     return null;
+  } catch (err) {
+    error = err.message;
+  } finally {
+    return { error, result };
+  }
+};
+
+export const deleteAvatar = async (avatar, user_id) => {
+  const sql = `DELETE avatar FROM users WHERE user_id = ?`;
+
+  let error = null;
+  let result = null;
+
+  try {
+    [result] = await query(sql, [avatar, user_id]);
+    if (result.affectedRows === 1) {
+      return true;
+    }
+    return false;
   } catch (err) {
     error = err.message;
   } finally {
