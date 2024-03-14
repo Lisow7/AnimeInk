1c73f85a9d9bf9c4dff7daa75615af823e5684fe
Pierre Maze <promazelaygue@gmail.com>
2024-03-14 15:42:56 +0100

    Add basic pages(Article, LoginRegister, Profile and import in file main.jsx

---
+++ AnimeInk/Frontend/src/pages/Article.jsx
@@ -0,0 +1,13 @@
+import Picture from "../components/Picture";
+
+const Article = () => {
+  return (
+    <>
+      <h1 className="start">🧾ARTICLE PAGE🧾</h1>
+      <Picture />
+      <p className="start"> All commentaries here !</p>;
+    </>
+  );
+};
+
+export default Article;
